d59f14fd9e9cdbb778829ac5254a5c1edfcb43ed
bnymnbgn <st4b@hotmail.com>
2025-09-01 03:06:47 +0200

    xxxx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import Hero from './components/sections/Hero';
-import Gallery from './components/sections/Gallery';
-import Services from './components/sections/Services';
-import Comparison from './components/sections/Comparison';
-import About from './components/sections/About';
-import Process from './components/sections/Process';
-import Pricing from './components/sections/Pricing';
-import FAQ from './components/sections/FAQ';
-import Contact from './components/sections/Contact';
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Hero from "./components/sections/Hero";
+import Gallery from "./components/sections/Gallery";
+import Services from "./components/sections/Services";
+import Comparison from "./components/sections/Comparison";
+import About from "./components/sections/About";
+import Process from "./components/sections/Process";
+import Pricing from "./components/sections/Pricing";
+import FAQ from "./components/sections/FAQ";
+import Contact from "./components/sections/Contact";
 
 function App() {
   return (
@@ -16,21 +16,21 @@ function App() {
       <Header />
       <main>
         <Hero />
-        
+
         <About />
-        
+
         <Gallery />
-        
+
         <Services />
-        
+
         <Comparison />
-        
+
         <Process />
-        
+
         <Pricing />
-        
+
         <FAQ />
-        
+
         <Contact />
       </main>
       <Footer />
