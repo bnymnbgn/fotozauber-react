b7d3dbd6e539f52b62ac3bf9927cdc95c5aa0121
bnymnbgn <st4b@hotmail.com>
2025-09-12 00:20:06 +0200

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-return [
-    'recipient_email' => 'info@noha-studio.de',
-    'smtp_host' => 'smtp.ionos.de',
-    'smtp_user' => 'info@noha-studio.de', // Deine E-Mail-Adresse bei Ionos
-    'smtp_pass' => 'Dclsl2thhcrsdcjb', // Dein E-Mail-Passwort
-    'smtp_port' => 587, // Oder 587 für TLS
-];
